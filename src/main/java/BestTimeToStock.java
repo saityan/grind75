e132f8682549d7dc32215766848a27b34eb3e30e
saityan <naminatosky@gmail.com>
2025-08-08 12:01:50 +0300

    Best time to buy and sell stock

---
+++ src/main/java/BestTimeToStock.java
@@ -0,0 +1,19 @@
+/**
+ * leetcode.com/problems/best-time-to-buy-and-sell-stock
+ */
+
+class BestTimeToStock {
+    public int maxProfit(int[] prices) {
+        if (prices.length == 0) return 0;
+
+        int minPrice = prices[0];
+        int maxProfit = 0;
+
+        for (int price: prices) {
+            maxProfit = Math.max(maxProfit, price - minPrice);
+            minPrice = Math.min(minPrice, price);
+        }
+
+        return maxProfit;
+    }
+}
