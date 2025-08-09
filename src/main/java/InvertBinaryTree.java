def4ed3188af6966d085206b8efa038cd5314b2f
saityan <naminatosky@gmail.com>
2025-08-09 14:36:48 +0300

    Invert Binary Tree (BFS)

---
+++ src/main/java/InvertBinaryTree.java
@@ -0,0 +1,34 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * leetcode.com/problems/invert-binary-tree
+ */
+
+class InvertBinaryTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+
+            TreeNode temp = current.left;
+            current.left = current.right;
+            current.right = temp;
+
+            if (current.left != null) {
+                queue.offer(current.left);
+            }
+            if (current.right != null) {
+                queue.offer(current.right);
+            }
+        }
+
+        return root;
+    }
+}
