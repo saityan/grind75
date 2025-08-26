ad13f0abb3cc11590877760a8aac03183308098c
saityan <naminatosky@gmail.com>
2025-08-26 23:26:24 +0300

    Queue using two stacks

---
+++ src/main/java/MyQueue.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+/**
+ * leetcode.com/problems/implement-queue-using-stacks
+ */
+
+class MyQueue {
+    Stack<Integer> inputStack;
+    Stack<Integer> outputStack;
+
+    public MyQueue() {
+        inputStack = new Stack<>();
+        outputStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        inputStack.push(x);
+    }
+
+    public int pop() {
+        if (outputStack.isEmpty()) {
+            while (!inputStack.isEmpty()) {
+                outputStack.push(inputStack.pop());
+            }
+        }
+
+        return outputStack.pop();
+    }
+
+    public int peek() {
+        if (outputStack.isEmpty()) {
+            while (!inputStack.isEmpty()) {
+                outputStack.push(inputStack.pop());
+            }
+        }
+
+        return outputStack.peek();
+    }
+
+    public boolean empty() {
+        return inputStack.isEmpty() && outputStack.isEmpty();
+    }
+}
